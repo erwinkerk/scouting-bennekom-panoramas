135e6065b3b0bc38b502932a475b2f621ea5ef3a
Erwin Kerk <erwin@blikgooien.nl>
2023-08-05 15:25:08 +0200

    added gallery order

--- panoStudioViewerGallery.js
+++ panoStudioViewerGallery.js
@@ -3,6 +3,62 @@ PanoramaStudioViewerParams = {
 {
     "gallery": [
         {
+            "href": "selterskampweg.js",
+            "location": {
+                "heading": 74.33
+            },
+            "text": "Selterskampweg",
+            "thumbnail": "selterskampweg_tiles/selterskampweg_thumb.jpg"
+        },
+        {
+            "href": "inrit.js",
+            "location": {
+                "heading": 312.48
+            },
+            "text": "Inrit",
+            "thumbnail": "inrit_tiles/inrit_thumb.jpg"
+        },
+        {
+            "href": "pad.js",
+            "location": {
+                "heading": 289.42
+            },
+            "text": "Pad",
+            "thumbnail": "pad_tiles/pad_thumb.jpg"
+        },
+        {
+            "href": "splitsing.js",
+            "location": {
+                "heading": 90.16
+            },
+            "text": "Splitsing",
+            "thumbnail": "splitsing_tiles/splitsing_thumb.jpg"
+        },
+        {
+            "href": "middenterrein-1.js",
+            "location": {
+                "heading": 81.23
+            },
+            "text": "Middenterrein 1",
+            "thumbnail": "middenterrein-1_tiles/middenterrein-1_thumb.jpg"
+        },
+        {
+            "href": "buiten.js",
+            "location": {
+                "heading": 2.57
+            },
+            "text": "Buitenterrein",
+            "thumbnail": "buiten_tiles/buiten_thumb.jpg"
+        },
+        {
+            "href": "kampvuurkuil.js",
+            "location": {
+                "heading": 113.12
+            },
+            "text": "Kampvuurkuil",
+            "thumbnail": "kampvuurkuil_tiles/kampvuurkuil_thumb.jpg"
+        },
+        {
             "href": "beukenbos-1.js",
             "location": {
                 "heading": 21.69
@@ -15,62 +71,7 @@ PanoramaStudioViewerParams = {
             "text": "Beukenbos",
             "thumbnail": "beukenbos-2_tiles/beukenbos-2_thumb.jpg"
         },
-        {
-            "href": "buiten.js",
-            "location": {
-                "heading": 2.57
-            },
-            "text": "Buitenterrein",
-            "thumbnail": "buiten_tiles/buiten_thumb.jpg"
-        },
-        {
-            "href": "inrit.js",
-            "location": {
-                "heading": 312.48
-            },
-            "text": "Inrit",
-            "thumbnail": "inrit_tiles/inrit_thumb.jpg"
-        },
-        {
-            "href": "kampvuurkuil.js",
-            "location": {
-                "heading": 113.12
-            },
-            "text": "Kampvuurkuil",
-            "thumbnail": "kampvuurkuil_tiles/kampvuurkuil_thumb.jpg"
-        },
-        {
-            "href": "middenterrein-1.js",
-            "location": {
-                "heading": 81.23
-            },
-            "text": "Middenterrein 1",
-            "thumbnail": "middenterrein-1_tiles/middenterrein-1_thumb.jpg"
-        },
-        {
-            "href": "pad.js",
-            "location": {
-                "heading": 289.42
-            },
-            "text": "Pad",
-            "thumbnail": "pad_tiles/pad_thumb.jpg"
-        },
-        {
-            "href": "selterskampweg.js",
-            "location": {
-                "heading": 74.33
-            },
-            "text": "Selterskampweg",
-            "thumbnail": "selterskampweg_tiles/selterskampweg_thumb.jpg"
-        },
-        {
-            "href": "splitsing.js",
-            "location": {
-                "heading": 90.16
-            },
-            "text": "Splitsing",
-            "thumbnail": "splitsing_tiles/splitsing_thumb.jpg"
-        }
+        
     ]
 }
 
