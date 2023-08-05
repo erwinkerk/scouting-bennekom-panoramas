3682fc5729f8e78dbb26577aadb896e77781b9e2
Erwin Kerk <erwin@blikgooien.nl>
2023-08-05 14:09:10 +0200

    added beukenbos, buiten welpen en kampvuurkuil

---
+++ panoStudioViewerGallery.js
@@ -0,0 +1,29 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer":
+{
+    "gallery": [
+        {
+            "href": "beukenbos.js",
+            "text": "Beukenbos",
+            "thumbnail": "beukenbos_tiles/beukenbos_thumb.jpg"
+        },
+        {
+            "href": "kampvuurkuil.js",
+            "location": {
+                "heading": 113.12
+            },
+            "text": "Kampvuurkuil",
+            "thumbnail": "kampvuurkuil_tiles/kampvuurkuil_thumb.jpg"
+        },
+        {
+            "href": "welpen-buiten.js",
+            "location": {
+                "heading": 2.57
+            },
+            "text": "Buitenterrein",
+            "thumbnail": "welpen-buiten_tiles/welpen-buiten_thumb.jpg"
+        }
+    ]
+}
+
+}
---
+++ welpen-buiten.js
@@ -0,0 +1,212 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);"
+    },
+"include": [
+        {
+            "src": "welpen-buiten_controls.js"
+        },
+        {
+            "src": "panoStudioViewerGallery.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 2,
+	        "restart": 5,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 59.96046965521103,
+	        "minpan": 0,
+	        "mintilt": -46.53445788544708
+	    },
+	"hotspot": [
+	        {
+	            "animation": {
+	                "duration": 1.28,
+	                "frames": 32,
+	                "height": 64,
+	                "width": 64
+	            },
+	            "bitmap": "pap_rings.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "javascript:",
+	            "hsinfo": {
+	                "align": "center",
+	                "captureMouse": true,
+	                "checkHover": true,
+	                "id": "hsinfo0",
+	                "maxwidth": "66%",
+	                "onclick": "function(){ this.viewer.action('hideMessage'); } ",
+	                "style": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);",
+	                "text": "<div style='padding: 16px;'>Welpen</div>",
+	                "xoff": 0,
+	                "yoff": -30
+	            },
+	            "onclick": "function(){ this.viewer.action('toggleMessage',this.hsinfo); }",
+	            "opacity": "1.00;1.00",
+	            "position": "108.3539,-0.1704",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.28,
+	                "frames": 32,
+	                "height": 64,
+	                "width": 64
+	            },
+	            "bitmap": "pap_rings.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "javascript:",
+	            "hsinfo": {
+	                "align": "center",
+	                "captureMouse": true,
+	                "checkHover": true,
+	                "id": "hsinfo1",
+	                "maxwidth": "66%",
+	                "onclick": "function(){ this.viewer.action('hideMessage'); } ",
+	                "style": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);",
+	                "text": "<div style='padding: 16px;'>Shanti</div>",
+	                "xoff": 0,
+	                "yoff": -30
+	            },
+	            "onclick": "function(){ this.viewer.action('toggleMessage',this.hsinfo); }",
+	            "opacity": "1.00;1.00",
+	            "position": "140.8112,-0.9025",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.28,
+	                "frames": 32,
+	                "height": 64,
+	                "width": 64
+	            },
+	            "bitmap": "pap_rings.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "",
+	            "opacity": "1.00;1.00",
+	            "position": "180.1017,-0.1704",
+	            "target": "",
+	            "textbox": {
+	                "align": "top",
+	                "style": "color:#ffffff; font-family: Arial, Helvetica, sans-serif; font-size: 12px; font-weight: normal; text-align:center; display: inline;",
+	                "text": "Padvinsters"
+	            },
+	            "transition": "zoomin,2;blend,2",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.0667,
+	                "frames": 32,
+	                "height": 42,
+	                "width": 102
+	            },
+	            "bitmap": "pap_perspectiveArrow.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "beukenbos.html",
+	            "opacity": "1.00;1.00",
+	            "position": "194.7441,4.4660",
+	            "target": "",
+	            "transition": "zoomin,2;blend,2",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.0667,
+	                "frames": 32,
+	                "height": 42,
+	                "width": 102
+	            },
+	            "bitmap": "pap_perspectiveArrow.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "javascript:",
+	            "hsinfo": {
+	                "align": "center",
+	                "captureMouse": true,
+	                "checkHover": true,
+	                "id": "hsinfo4",
+	                "maxwidth": "66%",
+	                "onclick": "function(){ this.viewer.action('hideMessage'); } ",
+	                "style": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);",
+	                "text": "<div style='padding: 16px;'>Middenterrein</div>",
+	                "xoff": 0,
+	                "yoff": -30
+	            },
+	            "onclick": "function(){ this.viewer.action('toggleMessage',this.hsinfo); }",
+	            "opacity": "1.00;1.00",
+	            "position": "86.6343,3.7339",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.0667,
+	                "frames": 32,
+	                "height": 34,
+	                "width": 72
+	            },
+	            "bitmap": "pap_runningArrowLeft.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "javascript:",
+	            "hsinfo": {
+	                "align": "center",
+	                "captureMouse": true,
+	                "checkHover": true,
+	                "id": "hsinfo5",
+	                "maxwidth": "66%",
+	                "onclick": "function(){ this.viewer.action('hideMessage'); } ",
+	                "style": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);",
+	                "text": "<div style='padding: 16px;'>Splitsing</div>",
+	                "xoff": 0,
+	                "yoff": -30
+	            },
+	            "onclick": "function(){ this.viewer.action('toggleMessage',this.hsinfo); }",
+	            "opacity": "1.00;1.00",
+	            "position": "61.9862,3.7339",
+	            "unit": "deg"
+	        }
+	    ],
+	"hotspots": {
+	        "visible": true
+	    },
+	"id": "welpen-buiten",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"welpen-buiten_tiles/welpen-buiten_t_1b_%0y_%0x.jpg"},"down":{"src":"welpen-buiten_tiles/welpen-buiten_t_1d_%0y_%0x.jpg"},"front":{"src":"welpen-buiten_tiles/welpen-buiten_t_1f_%0y_%0x.jpg"},"height":5634,"left":{"src":"welpen-buiten_tiles/welpen-buiten_t_1l_%0y_%0x.jpg"},"right":{"src":"welpen-buiten_tiles/welpen-buiten_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"welpen-buiten_tiles/welpen-buiten_t_1u_%0y_%0x.jpg"},"width":5634},{"back":{"src":"welpen-buiten_tiles/welpen-buiten_t_2b_%0y_%0x.jpg"},"down":{"src":"welpen-buiten_tiles/welpen-buiten_t_2d_%0y_%0x.jpg"},"front":{"src":"welpen-buiten_tiles/welpen-buiten_t_2f_%0y_%0x.jpg"},"height":2817,"left":{"src":"welpen-buiten_tiles/welpen-buiten_t_2l_%0y_%0x.jpg"},"right":{"src":"welpen-buiten_tiles/welpen-buiten_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"welpen-buiten_tiles/welpen-buiten_t_2u_%0y_%0x.jpg"},"width":2817},{"back":{"src":"welpen-buiten_tiles/welpen-buiten_t_3b_%0y_%0x.jpg"},"down":{"src":"welpen-buiten_tiles/welpen-buiten_t_3d_%0y_%0x.jpg"},"front":{"src":"welpen-buiten_tiles/welpen-buiten_t_3f_%0y_%0x.jpg"},"height":1409,"left":{"src":"welpen-buiten_tiles/welpen-buiten_t_3l_%0y_%0x.jpg"},"right":{"src":"welpen-buiten_tiles/welpen-buiten_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"welpen-buiten_tiles/welpen-buiten_t_3u_%0y_%0x.jpg"},"width":1409},{"back":{"src":"welpen-buiten_tiles/welpen-buiten_t_4b_%0y_%0x.jpg"},"down":{"src":"welpen-buiten_tiles/welpen-buiten_t_4d_%0y_%0x.jpg"},"front":{"src":"welpen-buiten_tiles/welpen-buiten_t_4f_%0y_%0x.jpg"},"height":705,"left":{"src":"welpen-buiten_tiles/welpen-buiten_t_4l_%0y_%0x.jpg"},"right":{"src":"welpen-buiten_tiles/welpen-buiten_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"welpen-buiten_tiles/welpen-buiten_t_4u_%0y_%0x.jpg"},"width":705},{"back":{"src":"welpen-buiten_tiles/welpen-buiten_t_5b_%0y_%0x.jpg"},"down":{"src":"welpen-buiten_tiles/welpen-buiten_t_5d_%0y_%0x.jpg"},"front":{"src":"welpen-buiten_tiles/welpen-buiten_t_5f_%0y_%0x.jpg"},"height":353,"left":{"src":"welpen-buiten_tiles/welpen-buiten_t_5l_%0y_%0x.jpg"},"right":{"src":"welpen-buiten_tiles/welpen-buiten_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"welpen-buiten_tiles/welpen-buiten_t_5u_%0y_%0x.jpg"},"width":353}],"multilevel":true,"preview":{"src":"welpen-buiten_tiles/welpen-buiten_preview.jpg"},"projection":"cubic"},
+	"location": {
+	        "heading": 2.57
+	    },
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "Buitenterrein Welpen",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "shortdescription": "Buitenterrein",
+	        "tilt": 0
+	    }
+},
+"version": "4.1"
+}
+}
