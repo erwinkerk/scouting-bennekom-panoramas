f58a86c4f5a7dda4249c445a171a6ac29b896c2e
Erwin Kerk <erwin@blikgooien.nl>
2023-08-05 14:24:33 +0200

    added buiten

---
+++ middenterrein-1.js
@@ -0,0 +1,140 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);"
+    },
+"include": [
+        {
+            "src": "middenterrein-1_controls.js"
+        },
+        {
+            "src": "panoStudioViewerGallery.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 2,
+	        "restart": 5,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 360,
+	        "maxtilt": 52.62411046263499,
+	        "minpan": 0,
+	        "mintilt": -41.245580922240784
+	    },
+	"hotspot": [
+	        {
+	            "animation": {
+	                "duration": 1.28,
+	                "frames": 32,
+	                "height": 64,
+	                "width": 64
+	            },
+	            "bitmap": "pap_rings.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "javascript:",
+	            "hsinfo": {
+	                "align": "center",
+	                "captureMouse": true,
+	                "checkHover": true,
+	                "id": "hsinfo0",
+	                "maxwidth": "66%",
+	                "onclick": "function(){ this.viewer.action('hideMessage'); } ",
+	                "style": "min-width: 25%; text-shadow: none; line-height: 100%; color:#000; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 400; text-align:left; border-radius: 3px; border: rgba(0,0,0,0.66) solid 1px; box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.66);background: rgba(255,255,255,0.75);",
+	                "text": "<div style='padding: 16px;'>Hogan</div>",
+	                "xoff": 0,
+	                "yoff": -30
+	            },
+	            "onclick": "function(){ this.viewer.action('toggleMessage',this.hsinfo); }",
+	            "opacity": "1.00;1.00",
+	            "position": "59.2250,0.3565",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.28,
+	                "frames": 32,
+	                "height": 64,
+	                "width": 64
+	            },
+	            "bitmap": "pap_rings.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "bevers.html",
+	            "opacity": "1.00;1.00",
+	            "position": "135.5812,0.1117",
+	            "target": "",
+	            "transition": "zoomin,2;blend,2",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.28,
+	                "frames": 32,
+	                "height": 64,
+	                "width": 64
+	            },
+	            "bitmap": "pap_rings.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "kampvuurkuil.html",
+	            "opacity": "1.00;1.00",
+	            "position": "81.9850,1.3353",
+	            "target": "",
+	            "transition": "zoomin,2;blend,2",
+	            "unit": "deg"
+	        },
+	        {
+	            "animation": {
+	                "duration": 1.0667,
+	                "frames": 32,
+	                "height": 34,
+	                "width": 72
+	            },
+	            "bitmap": "pap_runningArrowRight.png",
+	            "bitmapscale": "1.0",
+	            "bitmaptype": "floating",
+	            "href": "pad-splitsing.html",
+	            "opacity": "1.00;1.00",
+	            "position": "194.8063,11.3688",
+	            "target": "",
+	            "transition": "zoomin,2;blend,2",
+	            "unit": "deg"
+	        }
+	    ],
+	"hotspots": {
+	        "visible": true
+	    },
+	"id": "middenterrein-1",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"middenterrein-1_tiles/middenterrein-1_t_1b_%0y_%0x.jpg"},"down":{"src":"middenterrein-1_tiles/middenterrein-1_t_1d_%0y_%0x.jpg"},"front":{"src":"middenterrein-1_tiles/middenterrein-1_t_1f_%0y_%0x.jpg"},"height":5618,"left":{"src":"middenterrein-1_tiles/middenterrein-1_t_1l_%0y_%0x.jpg"},"right":{"src":"middenterrein-1_tiles/middenterrein-1_t_1r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"middenterrein-1_tiles/middenterrein-1_t_1u_%0y_%0x.jpg"},"width":5618},{"back":{"src":"middenterrein-1_tiles/middenterrein-1_t_2b_%0y_%0x.jpg"},"down":{"src":"middenterrein-1_tiles/middenterrein-1_t_2d_%0y_%0x.jpg"},"front":{"src":"middenterrein-1_tiles/middenterrein-1_t_2f_%0y_%0x.jpg"},"height":2809,"left":{"src":"middenterrein-1_tiles/middenterrein-1_t_2l_%0y_%0x.jpg"},"right":{"src":"middenterrein-1_tiles/middenterrein-1_t_2r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"middenterrein-1_tiles/middenterrein-1_t_2u_%0y_%0x.jpg"},"width":2809},{"back":{"src":"middenterrein-1_tiles/middenterrein-1_t_3b_%0y_%0x.jpg"},"down":{"src":"middenterrein-1_tiles/middenterrein-1_t_3d_%0y_%0x.jpg"},"front":{"src":"middenterrein-1_tiles/middenterrein-1_t_3f_%0y_%0x.jpg"},"height":1405,"left":{"src":"middenterrein-1_tiles/middenterrein-1_t_3l_%0y_%0x.jpg"},"right":{"src":"middenterrein-1_tiles/middenterrein-1_t_3r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"middenterrein-1_tiles/middenterrein-1_t_3u_%0y_%0x.jpg"},"width":1405},{"back":{"src":"middenterrein-1_tiles/middenterrein-1_t_4b_%0y_%0x.jpg"},"down":{"src":"middenterrein-1_tiles/middenterrein-1_t_4d_%0y_%0x.jpg"},"front":{"src":"middenterrein-1_tiles/middenterrein-1_t_4f_%0y_%0x.jpg"},"height":703,"left":{"src":"middenterrein-1_tiles/middenterrein-1_t_4l_%0y_%0x.jpg"},"right":{"src":"middenterrein-1_tiles/middenterrein-1_t_4r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"middenterrein-1_tiles/middenterrein-1_t_4u_%0y_%0x.jpg"},"width":703},{"back":{"src":"middenterrein-1_tiles/middenterrein-1_t_5b_%0y_%0x.jpg"},"down":{"src":"middenterrein-1_tiles/middenterrein-1_t_5d_%0y_%0x.jpg"},"front":{"src":"middenterrein-1_tiles/middenterrein-1_t_5f_%0y_%0x.jpg"},"height":352,"left":{"src":"middenterrein-1_tiles/middenterrein-1_t_5l_%0y_%0x.jpg"},"right":{"src":"middenterrein-1_tiles/middenterrein-1_t_5r_%0y_%0x.jpg"},"tilesize":480,"up":{"src":"middenterrein-1_tiles/middenterrein-1_t_5u_%0y_%0x.jpg"},"width":352}],"multilevel":true,"preview":{"src":"middenterrein-1_tiles/middenterrein-1_preview.jpg"},"projection":"cubic"},
+	"location": {
+	        "heading": 81.23
+	    },
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "Middenterrein 1",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "shortdescription": "Middenterrein 1",
+	        "tilt": 0
+	    }
+},
+"version": "4.1"
+}
+}
--- panoStudioViewerGallery.js
+++ panoStudioViewerGallery.js
@@ -3,6 +3,14 @@ PanoramaStudioViewerParams = {
 {
     "gallery": [
         {
+            "href": "middenterrein-1.js",
+            "location": {
+                "heading": 81.23
+            },
+            "text": "Middenterrein 1",
+            "thumbnail": "middenterrein-1_tiles/middenterrein-1_thumb.jpg"
+        },
+        {
             "href": "beukenbos.js",
             "text": "Beukenbos",
             "thumbnail": "beukenbos_tiles/beukenbos_thumb.jpg"
@@ -14,6 +22,14 @@ PanoramaStudioViewerParams = {
             },
             "text": "Kampvuurkuil",
             "thumbnail": "kampvuurkuil_tiles/kampvuurkuil_thumb.jpg"
+        },
+        {
+            "href": "pad-splitsing.js",
+            "location": {
+                "heading": 90.16
+            },
+            "text": "Pad bij Splitsing",
+            "thumbnail": "pad-splitsing_tiles/pad-splitsing_thumb.jpg"
         },
         {
             "href": "welpen-buiten.js",
